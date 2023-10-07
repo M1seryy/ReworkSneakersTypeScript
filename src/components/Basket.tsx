6ea856410a4ba6eb4cc951bcf7d80e2e68d74a7e
M1seryy <62029606+M1seryy@users.noreply.github.com>
2023-10-07 13:24:50 +0300

    basket

---
+++ src/components/Basket.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useRef, useState } from "react";
+import "../basket/basket.scss";
+import { ReactComponent as Plus } from "../../images/svg/plus.svg";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+
+const Basket = (
+    // { close, setClose, onDelete }
+    ) => {
+//   const dispatch = useDispatch();
+//   const addedArr = useSelector((state) => state.cardItems);
+//   const closeHandler = () => {
+//     setClose(!close);
+//   };
+//   const getBasket = async () => {
+//     const response = await axios.get(
+//       `https://63ef75c5c59531ccf16fa934.mockapi.io/basket`
+//     );
+//     dispatch(SET_BASKET(SET_CARD_ITEMS, response.data));
+//   };
+//   useEffect(() => {
+//     getBasket();
+//   }, [close]);
+
+  return (
+    <div className={"close" ? "overlay" : "overlay hidden"}>
+      <div className="basket-wrap">
+        <Plus  className="exit-btn"></Plus>
+        <h2 className="basket-title">Orders</h2>
+        <div className="cards">
+          {/* {addedArr.map((item, index) => {
+            return (
+              <BasketCards onDeleteFunc={onDelete} key={index} data={item} />
+            );
+          })} */}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Basket;
--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,12 +1,11 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import sneakerSlice from "../slices/sneakerSlice";
 
-
 export const store = configureStore({
   reducer: {
-sneakers:sneakerSlice
+    sneakers: sneakerSlice,
   },
 });
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
